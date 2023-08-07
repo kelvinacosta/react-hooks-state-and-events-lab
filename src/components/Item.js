edfa624d216b17031e3f97cd1d2822f81ab7f38b
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-08-07 00:00:33 -0400

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  
+  const [isInCart, setIsInCart] = useState("")
+
+  const appCart = isInCart ? "in-cart" : ""
+  const colorCart = isInCart ? "remove" : "add"
+  const changeTextContext = isInCart ? "Remove From Cart": "Add to Cart"
+
+  const handleClickCart = () => {
+    setIsInCart(!isInCart)
+  }
+  
   return (
-    <li className="">
+    <li className={appCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={colorCart} onClick={handleClickCart}>{changeTextContext}</button>
     </li>
   );
 }
